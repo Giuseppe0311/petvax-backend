7921f90ff73a933d6d45d381d4d2bc75f17562d1
angel giuseppe barrera romero <barrerita_romero@hotmail.com>
2024-07-09 14:08:51 -0500

    STARTING PROJECT petvax

---
+++ src/main/java/com/giuseppe/petvax/PetvaxApplication.java
@@ -0,0 +1,13 @@
+package com.giuseppe.petvax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetvaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetvaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giuseppe/petvax/PetvaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuseppe.petvax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetvaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
